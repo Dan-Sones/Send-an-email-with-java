f5d106835b9d02b5ff46861b230cc076e473a7b3
Daniel Sones <Daniel.sones@btinternet.com>
2020-05-01 20:57:05 +0100

    Add Java file

--- src/SendMailSSL.java
+++ src/SendMailSSL.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
 
-public class SendMailSSL {
+class Mailer{
+    public static void send(String from, String password, String to, String sub, String msg) {
+        //get properties object
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        // get session
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(from, password);
+                    }
+                });
+
+        //compose message
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(sub);
+            message.setText(msg);
+            // send message
+            Transport.send(message);
+            System.out.println("Message sent successfully!");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    } //method
+}// java mail class
+
+public class SendMailSSL{
+    public static void main(String [] args){
+        //ENTER YOUR DETAILS IN THE CORRECT ORDER AS ON THE LINE BELOW
+        //from, password,to,subject,message
+        Mailer.send("From@from.com","Password", "To@to.com","Subject", "Hey! I'm sending an email from java!");
+    }
+
 }
